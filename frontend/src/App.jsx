f84703dda8e6f70dac5890cf3a06ead72e0820cb
LucasHenry11 <henry.lucas0209@gmail.com>
2026-03-12 15:26:08 +0100

    feat: templates for pages to start the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import './App.css'
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <main classname = "main-content">
-      <Routes>
-        {/* insert routes here */}
-        {/*<Route path="/" element={<Home />} /> */}
-      </Routes>
-    </main>
+    <>
+      <main classname="main-content">
+        <Routes>
+          {/* insert routes here */}
+          {/*<Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </>
   )
 }
 
