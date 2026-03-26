2c2d67c12a43b57629d48297e732cbdaa7c5f073
Valentin Monod <vmonod5@gmail.com>
2026-03-26 18:44:29 +0100

    refactor(dependancies, navigation): update to react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
 import './css/App.css'
+import { Routes, Route } from "react-router";
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
+      <NavBar />
       <main className="main-content">
         <Routes>
           {/* insert routes here */}
