ecdfb03f90de6fd4d379c5201ee9dcb968569589
Valentin Monod <vmonod5@gmail.com>
2026-03-26 18:13:37 +0100

    fix(global): adjust structure and fix typo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <>
             <NavBar page="Home" />
-            <div classname="hero">
+            <div className="hero">
                 <h1>Don't Study Hard, Study Smart!</h1>
                 <p>Maximize your learning efficiency with our smart study platform that helps you stay organized.
                     All the tools you need for a comfortable and productive study experience, all in one place !</p>
