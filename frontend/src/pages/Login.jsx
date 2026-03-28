c765372f79fa864625f1e475f9621d05f91a900d
LucasHenry11 <henry.lucas0209@gmail.com>
2026-03-28 21:29:16 +0100

    feat: Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,24 @@
 import "../css/Login.css";
 
 export default function Login() {
+
+    function verifieEmail(e) {
+        e.preventDefault();
+        alert("TODO: Send email");
+    }
+
     return (
-        <>
-            TODO
-        </>
+        <div className="login-page">
+            <div className="login-container">
+                <h1><font color="#3400ff">L</font>ogin</h1>
+                <form className="login-form">
+                    <label htmlFor="email" id="label">
+                        Enter your email
+                    </label>
+                    <input type="email" id="email" name="email" required />
+                    <button className="send-mail" onClick={verifieEmail}>Check Email</button>
+                </form>
+            </div>
+        </div>
     )
 }
