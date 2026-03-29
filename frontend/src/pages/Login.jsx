204ecffd6e397c1b648d6ff3c5c3dd1ac368f65c
LucasHenry11 <henry.lucas0209@gmail.com>
2026-03-29 01:33:15 +0100

    feat(frontend): Updated Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,47 @@
 import "../css/Login.css";
+import { useState } from "react";
 
 export default function Login() {
+    const [emailSended, setEmailSended] = useState(false);
 
-    function verifieEmail(e) {
+    function verifieEmail(e) {  
         e.preventDefault();
         alert("TODO: Send email");
+        setEmailSended(true);  
+    }
+
+    if (emailSended) {          
+        return (
+            <div className="login-page">
+                <div className="login-container">
+                    <h1><span style={{ color: "#3400ff" }}>C</span>ode <span style={{ color: "#3400ff" }}>V</span>erification</h1>
+                    <form className="login-form">
+                        <label htmlFor="code">
+                            Enter the code sent to your email
+                        </label>
+                        <input type="text" id="code" name="code" required />
+                        <div className="button-container">
+                            <button onClick={() => setEmailSended(false)}>Back</button>
+                            <button onClick={() => alert("TODO: Verify code")}>Verify</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
     }
 
     return (
         <div className="login-page">
             <div className="login-container">
-                <h1><font color="#3400ff">L</font>ogin</h1>
+                <h1><span style={{ color: "#3400ff" }}>L</span>ogin</h1>
                 <form className="login-form">
-                    <label htmlFor="email" id="label">
-                        Enter your email
-                    </label>
+                    <label htmlFor="email">Enter your email</label>
                     <input type="email" id="email" name="email" required />
-                    <button className="send-mail" onClick={verifieEmail}>Check Email</button>
+                    <button className="send-mail" onClick={verifieEmail}>
+                        Send Email
+                    </button>
                 </form>
             </div>
         </div>
-    )
+    );
 }
