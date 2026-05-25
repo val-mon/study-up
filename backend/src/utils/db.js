7c2a2b61c654efeb2f45e5dd41c255c7be37c289
Valentin Monod <vmonod5@gmail.com>
2026-05-25 17:16:43 +0200

    fix(backend): change mongoose for mongdb native

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -1,12 +1,19 @@
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
+
+let client;
+let db;
 
 const connect = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  client = new MongoClient(process.env.MONGO_URI);
+  await client.connect();
+  db = client.db('studyup');
   console.log('INFO : Connected to MongoDB Atlas');
 };
 
 const disconnect = async () => {
-  await mongoose.disconnect();
+  await client.close();
 };
 
-module.exports = { connect, disconnect };
+const getDb = () => db;
+
+module.exports = { connect, disconnect, getDb };
